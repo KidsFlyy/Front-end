fe780f2de475767e7905cc03de1b47a49b1870f3
Nick Dailey <n.dailey000@icloud.com>
2019-10-17 22:42:37 -0500

    Janky buggy first run on the nav and admin login.

---
+++ front-end/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Navv from './components/Admin'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Navv /> 
+      </header>
+    </div>
+  );
+}
+
+export default App;
